2c75d448512046dbebc52a65a6e242f7e2301f10
agreat10 <komolov10@gmail.com>
2024-03-26 17:20:20 +0300

    First_Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: G:/CPP/Lessons
+# Build directory: G:/CPP/Lessons/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "G:/CPP/Lessons/CMakeLists.txt;74;add_test;G:/CPP/Lessons/CMakeLists.txt;0;")
